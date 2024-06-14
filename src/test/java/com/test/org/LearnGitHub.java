98f64ea688f7b9a464c53306d3a9ad5115147c87
sundarjikp <sundarjikpeee@gmail.com>
2024-06-14 23:50:47 +0530

    States were added

--- src/test/java/com/test/org/LearnGitHub.java
+++ src/test/java/com/test/org/LearnGitHub.java
@@ -14,4 +14,17 @@ public class LearnGitHub {
 		System.out.println("Method0");
 	}
 
+	private void nagaland() {
+		System.out.println("Method1");
+
+	}
+
+	private void sikkim() {
+		System.out.println("Method2");
+
+	}
+
+	private void Meghalaya() {
+		System.out.println("Method3");
+	}
 }
